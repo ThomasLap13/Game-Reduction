b0f20b07091ecdbf8f41d794cb37f6a6c06480f6
Lapierre Thomas <thomas.lapierre@coltene.com>
2025-04-25 10:00:56 +0200

    V2 - footer.php Ok

--- includes/footer.php
+++ includes/footer.php
@@ -3,21 +3,24 @@
     <footer>
         <div class="footer-container">
             <div class="footer-section about">
-                <h4><span style="font-weight: bold;">Thomas Lapierre</span></h4>
-                <p>Bienvenue sur mon portfolio ! Je suis un passionné de réseau et de création numérique, toujours à la recherche de nouvelles idées et défis à relever.</p>
-                <p>Avec une expertise en OPS, je m'efforce de créer des solutions innovantes et efficaces qui répondent aux besoins de mes clients et partenaires.</p>
-                <p>Explorez mes projets et découvrez comment je transforme des concepts en réalité. N'hésitez pas à me contacter pour discuter de nouvelles opportunités de collaboration !</p>
+                <h4><span style="font-weight: bold;">Game</span><span style="color: #36ae1b;">Réduction</span></h4>
+                <p>La meilleure plateforme pour économiser sur vos jeux vidéo et abonnements numériques.</p>
+                <p>Game Réduction n'est ni affilié, ni partenaire des entreprises et marques mentionnées sur ce site.</p>
+                <p>Les noms, logos et produits cités restent la propriété exclusive de leurs détenteurs respectifs.</p>
             </div>
     
             <div class="footer-section">
                 <h4>Navigation</h4>
                 <ul class="footer-links">
-                    <a href="../public/index.php" class="footer-link">Accueil</a><br>
+                    <a href="https://game-reduction.fr" class="footer-link">Accueil</a><br>
                     <a href="../public/a-propos.php" class="footer-link">À propos</a><br>
-                    <a href="Voir-plus/projets.php" class="footer-link">Projets</a><br>
-                    <a href="../public/evenements.php" class="footer-link">Événements</a><br>
-                    <a href="../public/veille.php" class="footer-link">Veille Technologique</a><br>
-                    <a href="../public/contact.php" class="footer-link">Contact</a><br>
+                    <a href="../public/shopping/jeux-video.php" class="footer-link">Jeux vidéo</a><br>
+                    <a href="../public/shopping/streaming.php" class="footer-link">Streaming</a><br>
+                    <a href="../public/shopping/microsoft.php" class="footer-link">Microsoft</a><br>
+                    <a href="../public/shopping/discord.php" class="footer-link">Discord</a><br>
+                    <a href="../public/shopping/logiciel.php" class="footer-link">Logiciel</a><br>
+                    <a href="../public/comment-commander.php" class="footer-link">Comment commander ?</a><br>
+                    <a href="../public//contact.php" class="footer-link">Contact</a><br>
                 </ul>
             </div>
     
@@ -32,17 +35,20 @@
             <div class="footer-section">
                 <h4>Réseaux sociaux</h4>
                 <div class="social-icons">
-                    <a href="https://www.linkedin.com/in/lapierre-thomas/" target="_blank" class="me-3">
-                        <i class="bi bi-linkedin" style="font-size: 1.5rem; color: #36ae1b;"></i>
+                    <a href="https://x.com/GameReduction" target="_blank" class="me-3">
+                        <i class="bi bi-twitter-x" style="font-size: 1.5rem; color: #36ae1b;"></i>
                     </a>
-                    <a href="https://github.com/thomaslap13" target="_blank" class="me-3">
-                        <i class="bi bi-github" style="font-size: 1.5rem; color: #36ae1b;"></i>
+                    <a href="https://discord.gg/mUGhG8S6Km" target="_blank" class="me-3">
+                        <i class="bi bi-discord" style="font-size: 1.5rem; color: #36ae1b;"></i>
+                    </a>
+                    <a href="https://www.youtube.com/@GameReduction" target="_blank">
+                        <i class="bi bi-youtube" style="font-size: 1.5rem; color: #36ae1b;"></i>
                     </a>
                 </div>
             </div>
         </div>
         <div class="footer-bottom">
-            &copy; 2025 Thomas Lapierre. Tous droits réservés.
+            &copy; 2025 Game Réduction. Tous droits réservés.
         </div>
     </footer>
     
