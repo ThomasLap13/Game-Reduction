30c4b7129b841280520b14c603cddbf86c48f2fe
Thomas <thomas25.lapierre@gmail.com>
2025-04-27 14:34:30 +0200

    V15 - Epic-Games.php Ok

---
+++ shopping/jeux-videos-pass-de-combat.php
@@ -0,0 +1,62 @@
+<?php
+include '../includes/header.php';
+?>
+
+<head>
+    <title>Passe de combat Fortnite - Game Réduction</title>
+</head>
+
+<main>
+    <div class="category-section nitro-benefits">
+        <div style="min-height: 50px;"></div>
+        <h2>Le Pass de Combat Fortnite : Nouvelles Récompenses et Défis</h2><hr>
+        <div class="container">
+            <div class="embed-responsive">
+                <iframe src="https://www.youtube.com/embed/zC8bquOTOzs"
+                        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+            </div>
+        </div><br><br>
+        <h2>Comment obtenir le Pass de Combat ?</h2>
+        <p>Le Pass de Combat Fortnite vous permet de débloquer de nombreuses récompenses exclusives tout au long de la saison.<br>Pour l'obtenir, il vous suffit de l'acheter directement via notre plateforme.</p><br>
+        <h2>Pourquoi acheter le Pass de Combat ?</h2>
+        <p>Le Pass de Combat Fortnite offre de nombreux avantages pour les joueurs :</p>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Avantage</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Accès à des skins exclusifs</td>
+                    <td>Des skins exclusifs et personnalisables pour votre personnage.</td>
+                </tr>
+                <tr>
+                    <td>Défis supplémentaires</td>
+                    <td>Des défis pour gagner de l'XP et des récompenses supplémentaires.</td>
+                </tr>
+                <tr>
+                    <td>Amélioration des compétences</td>
+                    <td>Un moyen d'améliorer vos compétences et de vous amuser avec des amis.</td>
+                </tr>
+                <tr>
+                    <td>Contenu renouvelé</td>
+                    <td>Un contenu renouvelé à chaque nouvelle saison pour garder l'excitation à son maximum.</td>
+                </tr>
+                <tr>
+                    <td>Engagement envers le jeu</td>
+                    <td>Montrez votre engagement envers le jeu et démarquez-vous dans les matchs.</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="text-center">
+            <a href="../public/comment-commander.php" class="btn btn-custom">Acheter maintenant</a>
+        </div>
+    </div>
+</main>
+
+<?php
+include '../includes/footer.php';
+?>
---
+++ shopping/jeux-videos-v-bucks.php
@@ -0,0 +1,55 @@
+<?php
+include '../includes/header.php';
+?>
+
+<head>
+    <title>V-Bucks - Game Réduction</title>
+</head>
+
+<main>
+<div class="category-section nitro-benefits">
+        <div style="min-height: 50px;"></div>
+        <h2>Nos offres V-Bucks</h2>
+        <hr>
+        <div class="container">
+            <div class="embed-responsive">
+                <iframe src="https://www.youtube.com/embed/zC8bquOTOzs"
+                        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+            </div>
+        </div>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Prix</th>
+                    <th>V-Bucks</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>💰 <s>89,99€</s> - 59,99€</td>
+                    <td>13500 V-Bucks.</td>
+                </tr>
+                <tr>
+                    <td>💰 <s>36,99€</s> - 26,99€</td>
+                    <td>5000 V-Bucks.</td>
+                </tr>
+                <tr>
+                    <td>💰 <s>22,99€</s> - 15,99€</td>
+                    <td>2800 V-Bucks.</td>
+                </tr>
+                <tr>
+                    <td>💰 <s>8,99€</s> - 6,99€</td>
+                    <td>1000 V-Bucks.</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="text-center">
+            <a href="../public/comment-commander.php" class="btn btn-custom">Acheter maintenant</a>
+        </div>
+    </div>
+</main>
+
+<?php
+include '../includes/footer.php';
+?>
